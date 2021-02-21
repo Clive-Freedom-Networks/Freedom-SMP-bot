c35c6edd969c250a28c8009aa9131b38abf610bd
Clive <finlaytaylor009@gmail.com>
2021-02-21 12:36:43 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Freedom is online')
+});
+
+
+
+client.login('ODEyODEzMDIwNjE5MDc5NzIw.YDGNHQ.IVVZd2LVezRxSzjOOKgu_aFTvhk');
